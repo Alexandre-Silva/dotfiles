600c17c7b8cd3f6f787a280aa0b04c34461d197c
Alexandre-Silva <aalexandre.grande@hotmail.com>
2015-03-02 16:52:27 +0000

    Bugfix(awesome rc): Integration with awesome3.5
    Fixed the xfce4_rc for the new version of awesome (3.5).
    Activated gradle plugin for ho-my-zsh.

--- config/awesome/xfce4_rc.lua
+++ config/awesome/xfce4_rc.lua
@@ -1,9 +1,9 @@
 -- Standard awesome library
-require("awful")
+local awful = require("awful")
 require("awful.autofocus")
-require("awful.rules")
+awful.rules = require("awful.rules")
 -- Theme handling library
-require("beautiful")
+local beautiful = require("beautiful")
 -- Notification library
 -- require("naughty")
 
@@ -53,8 +53,7 @@ editor_cmd = terminal .. " -e " .. editor
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
-layouts =
-{
+layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -66,7 +65,7 @@ layouts =
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
-    awful.layout.suit.floating
+    awful.layout.suit.floating,
 }
 -- }}}
 
@@ -115,8 +114,8 @@ mytaglist.buttons = awful.util.table.join(
                     awful.button({ modkey }, 1, awful.client.movetotag),
                     awful.button({ }, 3, awful.tag.viewtoggle),
                     awful.button({ modkey }, 3, awful.client.toggletag),
-                    awful.button({ }, 4, awful.tag.viewnext),
-                    awful.button({ }, 5, awful.tag.viewprev)
+                    awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+                    awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
                     )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
@@ -152,7 +151,7 @@ mytasklist.buttons = awful.util.table.join(
 
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
-    mypromptbox[s] = awful.widget.prompt({ layout = awful.widget.layout.horizontal.leftright })
+    mypromptbox[s] = awful.widget.prompt()
     -- Create an imagebox widget which will contains an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
     mylayoutbox[s] = awful.widget.layoutbox(s)
@@ -162,29 +161,33 @@ for s = 1, screen.count() do
                            awful.button({ }, 4, function () awful.layout.inc(layouts, 1) end),
                            awful.button({ }, 5, function () awful.layout.inc(layouts, -1) end)))
     -- Create a taglist widget
-    mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.label.all, mytaglist.buttons)
+    mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
 
     -- Create a tasklist widget
-    mytasklist[s] = awful.widget.tasklist(function(c)
-                                              return awful.widget.tasklist.label.currenttags(c, s)
-                                          end, mytasklist.buttons)
+    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
 
-   --  -- Create the wibox
-   --  mywibox[s] = awful.wibox({ position = "top", screen = s })
-   --  -- Add widgets to the wibox - order matters
-   --  mywibox[s].widgets = {
-   --      {
-   --          mylauncher,
-   --          mytaglist[s],
-   --          mypromptbox[s],
-   --          layout = awful.widget.layout.horizontal.leftright
-   --      },
-   --      mylayoutbox[s],
-   --      mytextclock,
-   --      s == 1 and mysystray or nil,
-   --      mytasklist[s],
-   --      layout = awful.widget.layout.horizontal.rightleft
-   --  }
+    -- Create the wibox
+    --mywibox[s] = awful.wibox({ position = "top", screen = s })
+
+    ---- Widgets that are aligned to the left
+    --local left_layout = wibox.layout.fixed.horizontal()
+    --left_layout:add(mylauncher)
+    --left_layout:add(mytaglist[s])
+    --left_layout:add(mypromptbox[s])
+
+    ---- Widgets that are aligned to the right
+    --local right_layout = wibox.layout.fixed.horizontal()
+    --if s == 1 then right_layout:add(wibox.widget.systray()) end
+    --right_layout:add(mytextclock)
+    --right_layout:add(mylayoutbox[s])
+
+    ---- Now bring it all together (with the tasklist in the middle)
+    --local layout = wibox.layout.align.horizontal()
+    --layout:set_left(left_layout)
+    --layout:set_middle(mytasklist[s])
+    --layout:set_right(right_layout)
+
+    --mywibox[s]:set_widget(layout)
 end
 -- }}}
 
@@ -345,7 +348,9 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "panel-1-whisker" },
       properties = { floating = true } },
-    
+    { rule = { instance = "plugin-container" },
+    properties = { floating = true } },    
+
 
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
