61fcdb892b68bddae0dc050d6f2381ee84086c60
Alex-Torre <alexandre.si.moes.lva@gmail.com>
2015-09-13 18:22:28 +0100

    Feat(emacs): add org-capture-templates, tags and todo sequences

--- emacs/layers/alex/packages.el
+++ emacs/layers/alex/packages.el
@@ -35,13 +35,64 @@ which require an initialization must be listed explicitly in the list.")
     :defer t
     :config
     (progn
-      (setq org-directory (expand-file-name "~/Documents/org"))
+      (setq org-directory (expand-file-name "~/org"))
       (setq org-default-notes-file (concat org-directory "/notes.org"))
 
       (setq org-startup-indented nil)
 
       (define-key global-map "\C-cc" 'org-capture)
       (global-set-key (kbd "\C-ca") 'org-agenda)
+
+
+      (setq org-capture-templates
+       (quote (("t" "todo" entry (file "~/gtd/refile.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/gtd/refile.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/gtd/refile.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/gtd/diary.org")
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/gtd/refile.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/gtd/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file "~/gtd/refile.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/gtd/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))     )
     )
+
+
+  (setq org-todo-keywords
+        (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+                (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+
+  (setq org-todo-keyword-faces
+        (quote (("TODO" :foreground "red" :weight bold)
+                ("NEXT" :foreground "blue" :weight bold)
+                ("DONE" :foreground "forest green" :weight bold)
+                ("WAITING" :foreground "orange" :weight bold)
+                ("HOLD" :foreground "magenta" :weight bold)
+                ("CANCELLED" :foreground "forest green" :weight bold)
+                ("MEETING" :foreground "forest green" :weight bold)
+                ("PHONE" :foreground "forest green" :weight bold))))
+
+
+                                        ; Tags with fast selection keys
+  (setq org-tag-alist (quote ((:startgroup)
+                              ("@work" . ?o)
+                              ("@home" . ?H)
+                              (:endgroup)
+                              ("WAITING" . ?w)
+                              ("HOLD" . ?h)
+                              ("LAZER" . ?l)
+                              ("WORK" . ?W)
+                              ("COMPUTA" . ?C)
+                              ("ORG" . ?O)
+                              ("NOTE" . ?n)
+                              ("CANCELLED" . ?c)
+                              ("FLAGGED" . ??))))
+
+
   )
-)
