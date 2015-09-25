3392c64f0bdfa45b501ea774e7fece4c96520ba2
Alexandre-Silva <aalexandre.grande@hotmail.com>
2015-09-25 12:47:38 +0100

    Feat(emacs): moved setq vars to .spacemacs

--- emacs/layers/alex/packages.el
+++ emacs/layers/alex/packages.el
@@ -33,7 +33,7 @@ which require an initialization must be listed explicitly in the list.")
 (defun alex/init-org ()
   (use-package org
     :defer t
-    :init
+    :config
     (progn
       (defun bh/find-project-task ()
         "Move point to the parent (project) task if any"
@@ -287,86 +287,8 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
           (if (bh/is-subproject-p)
               nil
             next-headline)))
-      )
-    :config
-    (progn
-      (setq org-directory (expand-file-name "~/org"))
-      (setq org-default-notes-file (concat org-directory "/notes.org"))
-
-      (setq org-agenda-files '("~/gtd"))
-
-      (setq org-startup-indented nil)
-
-      (define-key global-map "\C-cc" 'org-capture)
-      (global-set-key (kbd "\C-ca") 'org-agenda)
 
 
-      (setq org-capture-templates
-            (quote (("t" "todo" entry (file "~/gtd/refile.org")
-                     "* TODO %?\n%U\n\n" :clock-in t :clock-resume t)
-                    ("s" "someday" entry (file "~/gtd/refile.org")
-                     "* HOLD %?\n%U\n\n" :clock-in t :clock-resume t)
-                    ("r" "respond" entry (file "~/gtd/refile.org")
-                     "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n\n" :clock-in t :clock-resume t :immediate-finish t)
-                    ("n" "note" entry (file "~/gtd/refile.org")
-                     "* %? :NOTE:\n%U\n" :clock-in t :clock-resume t)
-                    ("j" "Journal" entry (file+datetree "~/gtd/diary.org")
-                     "* %?\n%U\n" :clock-in t :clock-resume t)
-                    ("w" "org-protocol" entry (file "~/gtd/refile.org")
-                     "* TODO Review %c\n%U\n" :immediate-finish t)
-                    ("m" "Meeting" entry (file "~/gtd/refile.org")
-                     "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-                    ("p" "Phone call" entry (file "~/gtd/refile.org")
-                     "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-                    ("h" "Habit" entry (file "~/gtd/refile.org")
-                     "* NEXT %?\n%U\nnSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
-      
-      
-      (setq org-todo-keywords
-            (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-                    (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
-
-      (setq org-todo-keyword-faces
-            (quote (("TODO" :foreground "red" :weight bold)
-                    ("NEXT" :foreground "OrangeRed" :weight bold)
-                    ("DONE" :foreground "forest green" :weight bold)
-                    ("WAITING" :foreground "orange" :weight bold)
-                    ("HOLD" :foreground "magenta" :weight bold)
-                    ("CANCELLED" :foreground "forest green" :weight bold)
-                    ("MEETING" :foreground "forest green" :weight bold)
-                    ("PHONE" :foreground "forest green" :weight bold))))
-
-      ;; Triggers that automatically assign tags to tasks based on state changes
-      (setq org-todo-state-tags-triggers
-            (quote (("CANCELLED" ("CANCELLED" . t))
-                    ("WAITING" ("WAITING" . t))
-                    ("HOLD" ("WAITING") ("HOLD" . t))
-                    (done ("WAITING") ("HOLD"))
-                    ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
-                    ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
-                    ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
-
-      ;; Tags with fast selection keys
-      (setq org-tag-alist (quote ((:startgroup)
-                                  ("@work" . ?o)
-                                  ("@home" . ?H)
-                                  (:endgroup)
-                                  ("WAITING" . ?w)
-                                  ("HOLD" . ?h)
-                                  ("LAZER" . ?l)
-                                  ("WORK" . ?W)
-                                  ("COMPUTA" . ?C)
-                                  ("ORG" . ?O)
-                                  ("NOTE" . ?n)
-                                  ("CANCELLED" . ?c)
-                                  ("READ" . ?r)
-                                  ("FLAGGED" . ??))))
-
-      ;; Do not dim blocked tasks
-      (setq org-agenda-dim-blocked-tasks nil)
-
-      ;; Compact the block agenda view
-      (setq org-agenda-compact-blocks t)
 
       ;; Custom agenda command definitions
       (setq org-agenda-custom-commands
@@ -442,40 +364,12 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
                              (org-tags-match-list-sublevels nil))))
                      nil))))
 
-                                        ; Targets include this file and any file contributing to the agenda - up to 9 levels deep
-      (setq org-refile-targets (quote ((nil :maxlevel . 9)
-                                       (org-agenda-files :maxlevel . 9))))
-
-                                        ; Use full outline paths for refile targets - we file directly with IDO
-      (setq org-refile-use-outline-path t)
-
-                                        ; Targets complete directly with IDO
-      (setq org-outline-path-complete-in-steps nil)
-
-                                        ; Allow refile to create parent tasks with confirmation
-      (setq org-refile-allow-creating-parent-nodes (quote confirm))
-
-                                        ; Use IDO for both buffer and file completion and ido-everywhere to t
-      ;;(setq org-completion-use-ido t)
-      ;;(setq ido-everywhere t)
-      ;;(setq ido-max-directory-size 100000)
-      ;;(ido-mode (quote both))
-
-      ;; Use the current window when visiting files and buffers with ido
-      ;;(setq ido-default-file-method 'selected-window)
-      ;;(setq ido-default-buffer-method 'selected-window)
-
-      ;; Use the current window for indirect buffer display
-      ;;(setq org-indirect-buffer-display 'current-window)
 
       ;; Refile settings
       ;; Exclude DONE state tasks from refile targets
       (defun bh/verify-refile-target ()
         "Exclude todo keywords with a done state from refile targets"
         (not (member (nth 2 (org-heading-components)) org-done-keywords)))
-
-      (setq org-refile-target-verify-function 'bh/verify-refile-target)
-
       )
     )
   )
