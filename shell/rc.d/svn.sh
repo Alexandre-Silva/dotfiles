4b70f34eab16bd1cdd031fee64f962fc40d035bc
Alexandre-Silva <aalexandre.grande@hotmail.com>
2015-12-30 19:02:59 +0000

    Feat(svn): default editor is now vim

---
+++ shell/rc.d/svn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: svn.sh
+# 
+#         USAGE: ./svn.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 21-12-2015 11:08
+#      REVISION:  ---
+#===============================================================================
+
+export SVN_EDITOR=vim
+
