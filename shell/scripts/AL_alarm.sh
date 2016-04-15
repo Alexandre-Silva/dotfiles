4c5d1fbd31783d5bc2d33f280f758a70c9f4e572
Alexandre-Silva <aalexandre.grande@hotmail.com>
2016-04-15 14:59:30 +0100

    feat(alarm): changed alarm period to 5 secs

--- shell/scripts/AL_alarm.sh
+++ shell/scripts/AL_alarm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 
-function BELL() { while true; do play -q /usr/share/sounds/freedesktop/stereo/bell.oga; sleep 2; done }
+function BELL() { while true; do play -q /usr/share/sounds/freedesktop/stereo/bell.oga; sleep 5; done }
 
 BELL &
 DISPLAY=:0 Xdialog --timeout "$1" --msgbox "$2" 0 0
