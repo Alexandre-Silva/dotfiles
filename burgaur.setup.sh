4181fdd888ee5bb21781f3c5172737244982e5de
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-06-01 22:53:37 +0100

    refactor(burgaur): rm control of tmp instal dir (ADM not handles it)

--- burgaur.setup.sh
+++ burgaur.setup.sh
@@ -7,13 +7,6 @@ AUR_COWER_GIT="https://aur.archlinux.org/cower.git"
 AUR_BURGAUR_GIT="https://aur.archlinux.org/burgaur.git"
 
 install() {
-    if [ -f /tmp/ADM ] || [ -d /tmp/ADM ]; then
-        error " /tmp/ADM already exists. Possibly previous installion did not exit safely."
-        return 1;
-    fi
-
-    mkdir /tmp/ADM && cd /tmp/ADM
-
     git clone $AUR_COWER_GIT
     cd cower
     makepkg --needed --noconfirm -sri
@@ -23,6 +16,4 @@ install() {
     cd burgaur
     makepkg --needed --noconfirm -sri
     cd ..
-
-    rm -rf /tmp/ADM
 }
