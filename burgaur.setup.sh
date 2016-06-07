22aaffb8c6aaa304f6d75039fbfa9bfed41d2bac
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-06-07 21:36:24 +0100

    feat(burgaur): add st_profile with configs env vars

--- burgaur.setup.sh
+++ burgaur.setup.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/bash
 
 packages=(
     "pm:base-devel"
+    "ranger"
 )
 
 AUR_COWER_GIT="https://aur.archlinux.org/cower.git"
 AUR_BURGAUR_GIT="https://aur.archlinux.org/burgaur.git"
 
-install() {
+function st_install() {
     git clone $AUR_COWER_GIT
     cd cower
     makepkg --needed --noconfirm -sri
@@ -16,4 +18,11 @@ install() {
     cd burgaur
     makepkg --needed --noconfirm -sri
     cd ..
+}
+
+function st_profile() {
+    # default
+    # export BURGAUR_TARGET_DIR="/var/tmp/"
+
+    export BURGAUR_FILE_MANAGER="ranger"
 }
