eb10f78cfe87de26db5c95be4a67c9fa69a63583
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-06-14 14:06:52 +0100

    chore(tmux): migrated to setup.sh

---
+++ setups/tmux/tmux.setup.sh
@@ -0,0 +1,13 @@
+packages=(
+    "pm:tmux"
+)
+
+links=(
+    "$DOTFILES/setups/tmux/tmux.conf" ~"/.tmux.conf"
+)
+
+function st_install() { return 0; }
+
+function st_profile() { return 0; }
+
+function st_rc() { return 0; }
