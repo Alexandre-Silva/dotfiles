8f9792a0b63a2ce250971641efdd6151fd80381b
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-07-07 16:06:52 +0100

    feat(burguar): if machine is arm then it installar cower using pacman

--- setups/burgaur.setup.sh
+++ setups/burgaur.setup.sh
@@ -9,10 +9,14 @@ AUR_COWER_GIT="https://aur.archlinux.org/cower.git"
 AUR_BURGAUR_GIT="https://aur.archlinux.org/burgaur.git"
 
 function st_install() {
-    git clone $AUR_COWER_GIT
-    cd cower
-    makepkg --needed --noconfirm -sri
-    cd ..
+    if [[ "$(uname -m)" =~ armv* ]]; then
+        sudo pacman -S cower;
+    else
+        git clone $AUR_COWER_GIT
+        cd cower
+        makepkg --needed --noconfirm -sri
+        cd ..
+    fi
 
     git clone $AUR_BURGAUR_GIT
     cd burgaur
