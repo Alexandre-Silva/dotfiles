8b08554af5276b84008d37536859358a311229ed
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-07-29 09:58:35 +0100

    feat(syncthing): add setup.sh

---
+++ setups/syncthing/syncthing.setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+packages=( "pm:syncthing"{,-gtk,-inotify} )
+
+__config="${XDG_CONFIG_HOME:-$HOME/.config}"
+links=(
+    {"$DOTFILES/setups/syncthing","$__config/systemd/user"}/syncthing.service
+)
