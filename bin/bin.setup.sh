f6111f3c08500344b47d579b7d5ef8069d90bc66
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-10-21 09:57:56 +0100

    refac(bin): rm unnecessary func in setup.sh

--- bin/bin.setup.sh
+++ bin/bin.setup.sh
@@ -2,11 +2,8 @@ packages=()
 
 links=()
 
-__compose_links() {
-    find "$DOTFILES/bin/sh/" -type f -executable -print0 | while IFS= read -r -d '' file; do
-        local link_name="$HOME/.bin/$(basename "$file")"
+find "$DOTFILES/bin/sh/" -type f -executable -print0 | while IFS= read -r -d '' file; do
+    local link_name="$HOME/.bin/$(basename "$file")"
 
-        links+=( "$file" "$link_name" )
-    done
-}
-__compose_links
+    links+=( "$file" "$link_name" )
+done
