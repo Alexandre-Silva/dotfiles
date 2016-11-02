5902418094e47af218252f4f09fd14047a31e6b9
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-11-02 10:09:44 +0000

    chore(rust): rm error message

--- setups/rust.setup.sh
+++ setups/rust.setup.sh
@@ -15,7 +15,5 @@ st_profile() {
     if [ -d "$cargo_bin" ]; then
         # verifies if path already contains "cargo_bin"
         [[ ! ":$PATH:" == *":$cargo_bin:"* ]] && export PATH="$PATH:$cargo_bin"
-    else
-        error "Cargo should be installed but $cargo_bin not found"
     fi
 }
