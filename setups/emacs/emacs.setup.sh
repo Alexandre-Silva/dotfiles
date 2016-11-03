ff8e5b49083e7fa23836a6e6f53ed35af6e49581
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-11-03 09:03:29 +0000

    chore(emacs): misc

--- setups/emacs/emacs.setup.sh
+++ setups/emacs/emacs.setup.sh
@@ -5,6 +5,7 @@ packages=(
     "pm:aspell"{,-en,-pt}
     "pm:yapf" # Python style guide checker
     "pm:clang-tools-extra"
+    "pm:cmake" # for ycmd
     "aur:libtinfo" # needed for ycmd
     "aur:global" # GNU tags. Source code tag system (use it to query a tags databse)
     "aur:universal-ctags-git" # used to actually create tags database (better than GNU tags)
--- setups/emacs/layers/alex/packages.el
+++ setups/emacs/layers/alex/packages.el
@@ -13,7 +13,6 @@
 (defvar alex-packages
   '(
     org
-    python
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
