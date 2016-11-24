a4ead7a53b0344eed4fc6351ac98fd874c31a870
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2016-11-24 08:51:22 +0000

    fix(canto): setup's link for sync-plugin was incorrect

--- setups/canto/setup.sh
+++ setups/canto/setup.sh
@@ -5,6 +5,6 @@ packages=(
 )
 
 links=(
-    {$DOTFILES"/setups/canto",$HOME"/.config"}/sync-rsync.py
+    {$DOTFILES"/setups/canto",$HOME"/.config/canto/plugins"}/sync-rsync.py
     {$DOTFILES"/setups/canto",$HOME"/.bin"}/canto-cursesl
 )
