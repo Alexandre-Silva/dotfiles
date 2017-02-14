1efae78e92741f5307f2039e886d8c8674aaede5
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-02-14 09:28:22 +0000

    chore(deluge): document what st_install does

--- setups/deluge.setup.sh
+++ setups/deluge.setup.sh
@@ -7,6 +7,12 @@ packages=(
 links=()
 
 st_install() {
+    # 1) Installs the plugin 'Default trackers' which adds a set of base
+    # trackers to all new torrents. THerefore, initial peer and seeder finding
+    # is much greater which minimizes warm-up period of a new torrent.
+    #
+    # 2) Adds a list of default trackers (after installing the plugin)
+
     local PLUGIN_DEFAULT_TRACKERS="https://github.com/stefantalpalaru/deluge-default-trackers"
     local deluge_conf="${XDG_CONFIG_DIRS:-$HOME/.config}/deluge"
     local deluge_plugins="$deluge_conf/plugins"
