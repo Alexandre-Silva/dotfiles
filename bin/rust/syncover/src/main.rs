5ca672816b0c5e43f43fe2c33f315af302ce82a3
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-03-06 15:16:54 +0000

    refac(syncover): format change

--- bin/rust/syncover/src/main.rs
+++ bin/rust/syncover/src/main.rs
@@ -4,16 +4,16 @@ extern crate time;
 extern crate regex;
 
 use std::io;
-use std::convert::AsRef;
 use std::path::{Path, PathBuf};
 use std::fs::{File, DirBuilder};
-use std::ops::Fn;
 use regex::Regex;
 use time::Tm;
 
 lazy_static! {
 static ref BCK_RE: Regex = Regex::new(r"^.*~([0-9]{8}-[0-9]{6}).*$").unwrap();
 }
+
+static BCK_TS: &'static str = "%Y%m%d-%H%M%S";
 
 #[derive(Debug)]
 struct Conf {
@@ -80,9 +80,10 @@ fn main() {
 
     let _ = find(c.bkp.as_path(),
                  &mut |pb: &Path| {
+
         let pb_str = pb.to_str().unwrap();
         if let Some(cap) = BCK_RE.captures(pb_str) {
-            if let Some(t) = time::strptime(&cap[1], "%Y%m%d-%H%M%S").ok() {
+            if let Some(t) = time::strptime(&cap[1], BCK_TS).ok() {
                 files_bkp.push(SyncFile::new(PathBuf::from(pb_str), t));
             }
         }
