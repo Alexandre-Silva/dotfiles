175d004857dd69cc116812e1837a652a182d59ed
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-03-06 21:49:41 +0000

    feat(syncover): stores files in hasmap

--- bin/rust/syncover/src/main.rs
+++ bin/rust/syncover/src/main.rs
@@ -4,6 +4,7 @@ extern crate time;
 extern crate regex;
 
 use std::ops::Deref;
+use std::string::String;
 use std::borrow::Cow;
 use std::borrow::Borrow;
 use std::collections::HashMap;
@@ -28,7 +29,7 @@ struct Conf {
 fn load_conf() -> Conf {
     Conf {
         local: PathBuf::from("/tmp/synconver/local"),
-        bkp: PathBuf::from("/tmp/alex/.stversions"),
+        bkp: PathBuf::from("/home/alex/dir-syncers/hdd-syncthing/.stversions"),
     }
 }
 
@@ -40,6 +41,7 @@ fn find<F>(file: &Path, cb: &mut F) -> io::Result<()>
         for entry in file.read_dir()? {
             let f = entry?.path();
             find(f.as_path(), cb)?;
+
         }
     } else if file.is_file() {
         cb(file);
@@ -72,6 +74,10 @@ impl SyncFile {
             bcks: Vec::new(),
         }
     }
+
+    fn push_bkp(&mut self, bkp: SyncFileBck) {
+        self.bcks.push(bkp)
+    }
 }
 
 #[derive(Debug)]
@@ -86,7 +92,7 @@ impl SyncFileBck {
     }
 }
 
-type FilesHM<'a> = HashMap<&'a str, SyncFile>;
+type FilesHM = HashMap<String, SyncFile>;
 
 fn main() {
     let c = load_conf();
@@ -101,25 +107,35 @@ fn main() {
              BCK_RE.replace("/tmp/alex/.stversions/gtd/mcast~20160509-091951.org", ""));
 
 
-    let files: FilesHM = HashMap::new();
+    let mut files: FilesHM = HashMap::new();
 
-    let _ = find(c.bkp.as_path(),
-                 &mut |pb: &Path| {
+    find(c.bkp.as_path(),
+         &mut |pb: &Path| {
 
         let pb_str = pb.to_str().unwrap();
         if let Some(cap) = BCK_RE.captures(pb_str) {
             if let Some(t) = time::strptime(&cap[1], BCK_TS).ok() {
 
                 let k = BCK_RE.replace(pb_str, "");
-                let mut e = files.entry(k.deref()).or_insert(SyncFile::new({
-                    let pb = PathBuf::new();
-                    pb.push(k.borrow());
-                    pb
-                }));
-                // files_bkp.push(SyncFile::new(PathBuf::from(pb_str), t));
-                // files.entry()
+
+                let sfb = SyncFileBck::new(PathBuf::from(pb_str.clone()), t);
+
+                if files.contains_key(k.deref()) {
+                    files.get_mut(k.deref()).unwrap().push_bkp(sfb);
+                } else {
+                    let mut sf = SyncFile::new(PathBuf::from(String::from(k)));
+                    sf.push_bkp(sfb);
+                    files.insert(String::from(sf.path.to_str().unwrap()), sf);
+                }
             }
         }
+
+        // let mut e = files.entry(k).or_insert(SyncFile::new(PathBuf::new()));
+        // files_bkp.push(SyncFile::new(PathBuf::from(pb_str), t));
+        // files.entry()
     });
+
+    // println!("{:?}", files);
+    println!("{:?}", files["/home/alex/dir-syncers/hdd-syncthing/.stversions/keepassDB.kdbx"])
 
 }
