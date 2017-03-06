c90b3e5094ef9839023b25d073d6cde330dcbfd3
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-03-06 19:09:54 +0000

    feat(syncover): store links to backups (WIP)

--- bin/rust/syncover/src/main.rs
+++ bin/rust/syncover/src/main.rs
@@ -3,6 +3,10 @@ extern crate lazy_static;
 extern crate time;
 extern crate regex;
 
+use std::ops::Deref;
+use std::borrow::Cow;
+use std::borrow::Borrow;
+use std::collections::HashMap;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::fs::{File, DirBuilder};
@@ -10,7 +14,7 @@ use regex::Regex;
 use time::Tm;
 
 lazy_static! {
-static ref BCK_RE: Regex = Regex::new(r"^.*~([0-9]{8}-[0-9]{6}).*$").unwrap();
+static ref BCK_RE: Regex = Regex::new(r"~([0-9]{8}-[0-9]{6})").unwrap();
 }
 
 static BCK_TS: &'static str = "%Y%m%d-%H%M%S";
@@ -57,15 +61,32 @@ fn test_setup(conf: &Conf) -> io::Result<()> {
 
 #[derive(Debug)]
 struct SyncFile {
-    bkp: PathBuf,
-    tm: Tm,
+    path: PathBuf,
+    bcks: Vec<SyncFileBck>,
 }
 
 impl SyncFile {
-    fn new(pb: PathBuf, tm: Tm) -> SyncFile {
-        SyncFile { bkp: pb, tm: tm }
+    fn new(pb: PathBuf) -> SyncFile {
+        SyncFile {
+            path: pb,
+            bcks: Vec::new(),
+        }
     }
 }
+
+#[derive(Debug)]
+struct SyncFileBck {
+    path: PathBuf,
+    tm: Tm,
+}
+
+impl SyncFileBck {
+    fn new(pb: PathBuf, tm: Tm) -> SyncFileBck {
+        SyncFileBck { path: pb, tm: tm }
+    }
+}
+
+type FilesHM<'a> = HashMap<&'a str, SyncFile>;
 
 fn main() {
     let c = load_conf();
@@ -76,7 +97,11 @@ fn main() {
         return;
     }
 
-    let mut files_bkp = Vec::new();
+    println!("{}",
+             BCK_RE.replace("/tmp/alex/.stversions/gtd/mcast~20160509-091951.org", ""));
+
+
+    let files: FilesHM = HashMap::new();
 
     let _ = find(c.bkp.as_path(),
                  &mut |pb: &Path| {
@@ -84,7 +109,15 @@ fn main() {
         let pb_str = pb.to_str().unwrap();
         if let Some(cap) = BCK_RE.captures(pb_str) {
             if let Some(t) = time::strptime(&cap[1], BCK_TS).ok() {
-                files_bkp.push(SyncFile::new(PathBuf::from(pb_str), t));
+
+                let k = BCK_RE.replace(pb_str, "");
+                let mut e = files.entry(k.deref()).or_insert(SyncFile::new({
+                    let pb = PathBuf::new();
+                    pb.push(k.borrow());
+                    pb
+                }));
+                // files_bkp.push(SyncFile::new(PathBuf::from(pb_str), t));
+                // files.entry()
             }
         }
     });
