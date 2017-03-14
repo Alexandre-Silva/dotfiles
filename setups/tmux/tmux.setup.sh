fbb477e1fce880ddfa29dede73440d9235e6c6ba
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-03-14 18:16:30 +0000

    chore: rm dead code

--- setups/tmux/tmux.setup.sh
+++ setups/tmux/tmux.setup.sh
@@ -6,9 +6,3 @@ links=(
     {"$DOTFILES/setups/tmux/",$HOME"/."}tmux.conf
     {"$DOTFILES/setups/tmux",$HOME"/.bin"}/tlist-keys.sh
 )
-
-function st_install() { return 0; }
-
-function st_profile() { return 0; }
-
-function st_rc() { return 0;}
