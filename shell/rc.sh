49c74fc7d5843431273e1805c60846d3850c46ae
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-03-16 19:11:30 +0000

    fix(shell): sometimes rc.d files were not sourced in the correct order

--- shell/rc.sh
+++ shell/rc.sh
@@ -6,6 +6,6 @@
 # for dependencies handling files in rc.d are loaded by 0-filenames.sh then 1-filenames.sh ...
 # files which do not start with '[0-9]-' are loaded last
 
-for f in $(find $DOTFILES"/shell/rc.d" -type f -name "*.sh" ); do
-    . $f
+for f in $DOTFILES"/shell/rc.d/"* ; do
+    source "${f}"
 done
