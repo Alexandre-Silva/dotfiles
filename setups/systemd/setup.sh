b74d80df93e1d6fb8ff34edacfb64b2c3a256e34
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-06-23 08:39:24 +0100

    feat(systemd): add setup for systemd confs (DONE)

--- setups/systemd/setup.sh
+++ setups/systemd/setup.sh
@@ -2,14 +2,9 @@
 
 packages=()
 
-links=("${DOTFILES}/")
+links=( {"${DOTFILES}/setups/systemd/","${HOME}/.config/systemd/user/"}secure-tunnel@.service )
 
-st_profile() {
-    export RUST_SRC_PATH="/usr/src/rust/src"
 
-    local cargo_bin="$HOME/.cargo/bin"
-    if [ -d "$cargo_bin" ]; then
-        # verifies if path already contains "cargo_bin"
-        [[ ! ":$PATH:" == *":$cargo_bin:"* ]] && export PATH="$PATH:$cargo_bin"
-    fi
+st_install() {
+    systemctl --user daemon-reload
 }
