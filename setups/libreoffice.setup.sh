dcdc7a8e2f580fc7613592a7037afd677e1d0452
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-07-27 14:26:44 +0100

    feat(libreoffice): create setup.sh

---
+++ setups/libreoffice.setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+packages=(
+    "pm:libreoffice-fresh"
+    "pm:hunspell"
+    "pm:hunspell-en"
+    "aur:hunspell-pt_pt"
+    "aur:hunspell-pt_pt-preao" # pre acordo ortográfico
+)
