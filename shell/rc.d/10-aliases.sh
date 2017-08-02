68add3c36a6cd4ea0ce0bb0ff69f914f26a397f7
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-08-02 10:20:37 +0100

    fix(shell): dircolors improperly set

--- shell/rc.d/10-aliases.sh
+++ shell/rc.d/10-aliases.sh
@@ -6,7 +6,6 @@
 
 # enable color support of ls and also add handy aliases
 if  hash dircolors &>/dev/null; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
