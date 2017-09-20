6ab0b057c069d0a4085caf1df43477a6498b6854
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-09-20 14:36:02 +0100

    feat(shell): add fast-syntax-higlignting (instead of deprecated version)

--- shell/rc.d/90-syntax-highlight.zsh
+++ shell/rc.d/90-syntax-highlight.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-# From: https://github.com/zsh-users/zsh-syntax-highlighting
+# From: https://github.com/zdharma/fast-syntax-highlighting
 # NOTE: This must be loaded AT THE end of all other crap
-try_source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+try_source "${DOTFILES}/shell/lib/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
