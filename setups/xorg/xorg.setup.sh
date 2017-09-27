070b4888379cdd6f78dab00e20e1ae1c2d682d94
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-09-27 11:12:58 +0100

    feat(xorg): add packages

--- setups/xorg/xorg.setup.sh
+++ setups/xorg/xorg.setup.sh
@@ -1,4 +1,9 @@
-packages=( )
+#!/usr/bin/env bash
+
+packages=(
+    "pm:xorg" # group, not actual package
+    "pm:xorg-drivers" # group, not actual package
+)
 
 links=(
     {"$DOTFILES/setups/xorg/",$HOME"/."}xinitrc
