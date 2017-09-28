671139419043b955662bf12288f085f6c43fd7b9
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-09-28 20:16:05 +0000

    fix(radicale): setup.sh has some bugs

--- setups/radicale/setup.sh
+++ setups/radicale/setup.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 packages=(
-    'pm:radicale'
+    'aur:radicale'
 
     # options
     'pm:python-passlib'
     'pm:python-requests'
-    'pm:python-dulwich'
 )
 
 links=( "${ADM_DIR}/config" "${HOME}/.config/radicale/config")
 
 st_install() {
-    sudo install -m644 -D "${ADM_DIR}/radicale@.service" '/etc/systemd/service/'
+    sudo install -m644 -D --owner=root --group=root "${ADM_DIR}/radicale@.service" '/etc/systemd/system/'
     sudo systemctl daemon-reload
     sudo systemctl enable "radicale@${USER}"
     sudo systemctl start "radicale@${USER}"
