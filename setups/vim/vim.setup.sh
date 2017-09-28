e9c6241127fe317a1d0f751b34029899220410eb
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-09-28 14:37:14 +0100

    feat(vim): upd8 for neovim (WIP)

--- setups/vim/vim.setup.sh
+++ setups/vim/vim.setup.sh
@@ -2,20 +2,23 @@
 
 packages=(
     "pm:neovim"
+    "pm:python-neovim"
 )
 
 links=(
-    $DOTFILES"/setups/vim/vimrc" $HOME"/.vimrc"
-    $DOTFILES"/setups/vim/gvimrc" $HOME"/.gvimrc"
+    # $DOTFILES"/setups/vim/vimrc" $HOME"/.vimrc"
+    # $DOTFILES"/setups/vim/gvimrc" $HOME"/.gvimrc"
 
-    $HOME/{.vimrc,.config/nvim/init.vim}
-    $HOME/{.gvimrc,.ngvimrc}
+    # $HOME/{.vimrc,.config/nvim/init.vim}
+    # $HOME/{.gvimrc,.ngvimrc}
+
+    "${ADM_DIR}/init.vim" $HOME"/.config/nvim/init.vim"
 )
 
-function st_install() { nvim -c "PluginUpdate" -c "quitall" ; }
+# function st_install() { nvim -c "PluginUpdate" -c "quitall" ; }
 
 function st_rc() {
-    alias vimrc="vim ~/.vimrc"
+    alias vimrc="nvim $HOME/.config/nvim/init.vim"
 
     if hash nvim &>/dev/null; then
         alias vi=nvim
