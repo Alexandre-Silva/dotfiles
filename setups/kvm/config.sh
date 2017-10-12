737599e3d693eb8689c28f98b0d4fafe7438444e
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-10-12 21:12:21 +0100

    feat(kvm): add setup.sh and configs

---
+++ setups/kvm/config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# There are several files involved in configuring a vm for passtrough:
+#
+# antergos.conf - /boot/loader/entries/antergos.conf - kernel parameters
+# modprobe.d/vfio.conf - bind kernel module to graphics card
+# mkinitcpio.conf - has configs for forcing the correct module load order
+
+packages=(
+    qemu libvirt ovmf # The VM and shit
+    virt-manager      # Desktop user interface for managing virtual machines
+    ebtables dnsmasq  # default NAT/DHCP networking.
+    bridge-utils      #  bridged networking.
+    gnu-netcat        # remote management over SSH.
+)
