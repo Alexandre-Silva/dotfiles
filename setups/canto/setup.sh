f4801c2553bba42f84a824f8629e8d6f4d987764
Alexandre-Silva <alexandre.si.moes.lva@gmail.com>
2017-10-15 13:00:57 +0100

    fix(cant); workaround for locale issue (i think)

--- setups/canto/setup.sh
+++ setups/canto/setup.sh
@@ -8,4 +8,5 @@ packages=(
 links=(
     {$DOTFILES"/setups/canto",$HOME"/.config/canto/plugins"}/sync-rsync.py
     {$DOTFILES"/setups/canto",$HOME"/.bin"}/canto-cursesl
+    {$DOTFILES"/setups/canto",$HOME"/.bin"}/canto-curses
 )
