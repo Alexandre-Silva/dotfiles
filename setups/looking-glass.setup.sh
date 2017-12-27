33c30a88eef0557876c4646edb89a5600bd1af16
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2017-12-27 21:16:49 +0000

    feat: add looking-glass setup

---
+++ setups/looking-glass.setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+packages=(
+    "pm:git"
+    "pm:sdl2"{,_ttf}
+    "pm:spice"{,-protocol}
+    "pm:openssl"
+    "pm:fontconfig"
+    "pm:libx11"
+    "pm:ttf-freefont"
+)
+
+st_install() {
+    echo "Installation is a bit hands on"
+    echo "see https://looking-glass.hostfission.com/quickstart"
+}
+
+st_rc() {
+    ivshmem-server-lk() {
+        local mem=$(python -c 'import math; print(math.ceil((1920 * 1080 * 4 * 2 + 4224) / (1024**2)))' )
+        ivshmem-server -p /tmp/ivshmem.pid -S /tmp/ivshmem_socket -l "${mem}M" -n 8 "$@"
+    }
+}
