4a90aea871b878f0883235c5a7531631b418a2a3
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2017-12-27 11:05:27 +0000

    feat(docker): add setup.sh

---
+++ setups/docker.setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+packages=( pm:docker )
+
+st_rc() {
+    alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
+    alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
+}
