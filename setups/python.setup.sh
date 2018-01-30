36a7d69967896024d19ce3b7b407873e57275f09
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2018-01-30 08:19:34 +0000

    feat(python): create unified setup.sh

--- setups/python.setup.sh
+++ setups/python.setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
 packages=(
+    "pm:python"
     "aur:pyenv"
 )
 
 st_profile() {
+    PATH="$(python -m site --user-base)/bin:${PATH}"
+
+    # setup pyenv
     export PYENV_ROOT="$HOME/.pyenv"
 
     if which pyenv > /dev/null; then
