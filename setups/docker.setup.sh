f87beb1fcbaed15c3b70050acaab71469f852f40
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2018-03-22 14:17:21 +0000

    feat(docker): improved docker configurations
    - overrides docker.service to use overlay (File System driver)
    - adds docker system prune service (with systemd.timer)
    - add $USER to docker group

--- setups/docker.setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-packages=( pm:docker )
-
-st_rc() {
-    alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
-    alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
-}
---
+++ setups/docker/setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+packages=( pm:docker )
+
+links=()
+
+for f in docker-system-prune.timer docker-system-prune.service; do
+    links+=( "${ADM_DIR}/${f}" ~/.config/systemd/user/"${f}" )
+done
+
+st_rc() {
+    alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
+    alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
+}
+
+st_install() {
+    md5sumv() { md5sum "$@" | cut -d ' ' -f 1; }
+
+    # see https://wiki.archlinux.org/index.php/Docker#Storage_driver
+    local override='override.conf'
+    local dir='/etc/systemd/system/docker.service.d'
+
+    if [ ! -e "${dir}/${override}" ]; then
+        sudo mkdir --parents /etc/systemd/system/docker.service.d/
+        sudo install --mode=a=r,u+w "${ADM_DIR}/${override}" "${dir}"
+        sudo systemctl daemon-reload
+
+    elif [ "$(md5sumv "${dir}/${override}")" != "$(md5sumv "${ADM_DIR}/${override}")" ]; then
+        warn docker/setup.sh: "${dir}/${override}" already exists skipping installation of "${override}"
+    fi
+
+    sudo usermod -aG docker "$USER"
+    systemctl --user daemon-reload
+    systemctl --user enable --now docker-system-prune.timer
+}
