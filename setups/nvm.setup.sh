887da2979260e89fcef5dd16ff2eed85333aac32
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2018-03-26 21:37:56 +0100

    feat(nvm): it's now lazily loaded

--- setups/nvm.setup.sh
+++ setups/nvm.setup.sh
@@ -2,5 +2,9 @@
 
 st_rc() {
     export NVM_DIR="/home/alex/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+    # lazy loads NVM
+    nvm() {
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    }
 }
