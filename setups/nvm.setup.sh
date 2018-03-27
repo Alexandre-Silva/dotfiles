3adc87e8b3d20c16a4c706a2030f2ab7cdec90dd
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2018-03-27 14:18:19 +0100

    fix(nvm): lazy load of `nvm` now executes nvm after loading

--- setups/nvm.setup.sh
+++ setups/nvm.setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 st_rc() {
-    export NVM_DIR="/home/alex/.nvm"
+    export NVM_DIR="${HOME}/.nvm"
 
     # lazy loads NVM
     nvm() {
         [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+        nvm "$@"
     }
 }
