4581e1b95c688f3155525c024b45b2fed5b6ff2d
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2018-04-24 15:17:43 +0100

    fonts: add moar fonts

--- setups/fonts/setup.sh
+++ setups/fonts/setup.sh
@@ -7,11 +7,11 @@ packages=(
     pm:{cairo,fontconfig,freetype2,jdk8-openjdk,jre8-openjdk,jre8-openjdk-headless}
     pm:ttf-freefont
     pm:ttf-linux-libertine
+    pm:awesome-terminal-fonts
 
     # aur:fonts-meta-extended-lt
     aur:nerd-fonts-complete
     aur:ttf-google-fonts-git
     aur:ttf-mac-fonts
     # aur:ttf-oxygen
-
 )
