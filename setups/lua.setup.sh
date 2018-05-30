34c20484ab414fd459f6ff2d497c94d4fb7f942d
Alexandre-Silva <alexandre.simoesilva@gmail.com>
2018-05-30 13:55:49 +0100

    lua: fix error caused when luarocks isen't installed

--- setups/lua.setup.sh
+++ setups/lua.setup.sh
@@ -8,5 +8,7 @@ packages=(
 st_profile() {
     # the subshell cmd outputs the something along the lines of: 'export
     # LUA_PATH=...' which is then evaluated.
-    hash luarocks &>/dev/null && eval "$(luarocks path --bin)"
+    if hash luarocks &>/dev/null; then
+        eval "$(luarocks path --bin)"
+    fi
 }
