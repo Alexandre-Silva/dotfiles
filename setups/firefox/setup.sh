da481afa3d13397094e115fbe21f66289fe4b6b7
Alexandre-Silva <alexandre.s.silva@posteo.net>
2018-09-18 09:06:31 +0100

    firefox: rm troublesome guards

--- setups/firefox/setup.sh
+++ setups/firefox/setup.sh
@@ -4,8 +4,6 @@ packages=(
     "pm:firefox"
 )
 
-adm_sh_setopt_push +nullglob
-
 # Only sets the userChrome if the firefox profile exists
 __FF_HOME=( ~/.mozilla/firefox/*.default )
 if [ "${__FF_HOME}" != '~/.mozilla/firefox/*.default' ]; then
@@ -13,5 +11,3 @@ if [ "${__FF_HOME}" != '~/.mozilla/firefox/*.default' ]; then
         "${ADM_DIR}/userChrome.css" "${__FF_HOME}/chrome/userChrome.css"
     )
 fi
-
-adm_sh_setopt_pop
