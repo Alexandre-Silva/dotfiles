7f0ccb0743c6bd2573558b8f1de8f2ae0310d5cf
Alexandre-Silva <alexandre.s.silva@posteo.net>
2019-01-16 08:14:40 +0000

    mangakakalot: fix pdf scrapper's hardcoded paths

--- setups/mangakakalot/mangakakalot-scrapper.sh
+++ setups/mangakakalot/mangakakalot-scrapper.sh
@@ -3,12 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-# tmp=$(mktemp -d /tmp/mangakakalot-scapper.XXXXXX)
-tmp=/tmp/mangakakalot-scapper.APaxZW
+tmp=$(mktemp -d /tmp/mangakakalot-scapper.XXXXXX)
+# tmp=/tmp/mangakakalot-scapper.APaxZW
 
 template="$1";shift
 out_dir="$1";shift
-python3 "$DOTFILES/bin/python/mangakakalot-scrapper.py" "$tmp" "$@"
+python3 "$DIR/mangakakalot-scrapper.py" "$tmp" "$@"
 
 for series in "$tmp/"* ; do
     for chapter in "$series/"* ; do
--- setups/mangakakalot/setup.sh
+++ setups/mangakakalot/setup.sh
@@ -6,4 +6,5 @@ links=(
     {"$ADM_DIR/","${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user/"}mangakakalot-rss.service
     {"$ADM_DIR/","$HOME/.bin/"}mangakakalot-rss
     {"$ADM_DIR/","$HOME/.bin/"}mangakakalot-scrapper.sh
+    {"$ADM_DIR/","$HOME/.bin/"}mangakakalot-scrapper.py
 )
