7ef57da2c0f8ed30cd0ba250c711cf29fb5366f8
Alexandre-Silva <alexandre.s.silva@posteo.net>
2019-09-26 08:41:39 +0100

    shell: add  pip-update-sys cmd

--- setups/python.setup.sh
+++ setups/python.setup.sh
@@ -68,4 +68,11 @@ st_rc() {
             | cut -d = -f 1 \
             | xargs -n1 pip install --user -U
     }
+
+    pip-update-sys() {
+        pip list --outdated --format=freeze \
+            | grep -v '^\-e' \
+            | cut -d = -f 1 \
+            | xargs -n1 sudo pip install -U
+    }
 }
