e92d44715f342e1edcce1f5ddbd5e948f1db547d
Alexandre-Silva <alexandre.s.silva@posteo.net>
2020-03-19 14:49:44 +0000

    xorg: add global press-to-speak key (in mouse b8)

--- setups/xorg/setup.sh
+++ setups/xorg/setup.sh
@@ -3,9 +3,11 @@
 packages=(
     "pm:xorg" # group, not actual package
     "pm:xorg-drivers" # group, not actual package
+    "pm:xbindkeys" # group, not actual package
 )
 
 links=(
     {"$DOTFILES/setups/xorg/",$HOME"/."}xinitrc
     {"$DOTFILES/setups/xorg/",$HOME"/."}Xresources
+    {"$DOTFILES/setups/xorg/",$HOME"/."}xbindkeysrc
 )
