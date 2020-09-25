d45dfa67f16b4cea17d985570a46b9e8b7662380
Alexandre-Silva <alexandre.s.silva@posteo.net>
2020-09-25 16:44:10 +0100

    archlinux: add pkg kernel-modules-hooks

--- setups/archlinux/setup.sh
+++ setups/archlinux/setup.sh
@@ -10,6 +10,14 @@ depends=(
 packages=(
     pm:pacman-contrib
     pm:pigz # drop in replacemente for gzip with parallel compression
+    aur:kernel-modules-hook
 )
 
 links=( "${ADM_DIR}/pacman" "${XDG_CONFIG_HOME:-$HOME/.config}/pacman" )
+
+
+st_install() {
+    # for kernel-modules-hook
+    sudo systemctl daemon-reload
+    sudo systemctl enable linux-modules-cleanup
+}
