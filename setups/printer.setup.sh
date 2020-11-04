d58a435e4dbdb960f6ebe53f0a052daf662a1ca9
Alexandre-Silva <alexandre.s.silva@posteo.net>
2020-11-04 08:31:25 +0000

    printer: add setup

---
+++ setups/printer.setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+packages=(
+    pm:hplip
+    pm:ipp-usb
+    pm:xsane
+    pm:rpcbind
+    pm:wget
+    pm:cups-pk-helper
+    pm:cups
+    pm:system-config-printer
+    pm:nss-mdns
+)
