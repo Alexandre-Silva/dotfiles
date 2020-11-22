9add8fdad0ee74ba3cacf5111fdc36d4e93e826e
Alexandre-Silva <alexandre.s.silva@posteo.net>
2020-11-22 13:51:20 +0000

    nnn: add config

---
+++ setups/nnn/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+packages=(
+    'pm:nnn'
+)
+
+links=( "${ADM_DIR}" "CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/nnn/" )
+
+st_install() {
+    curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
+}
+
+st_rc() {
+    lastcd='/usr/share/nnn/quitcd/quitcd.bash_zsh'
+    [ -f "${lastcd}" ] && source "${lastcd}"
+}
--- setups/radicale/setup.sh
+++ setups/radicale/setup.sh
@@ -8,7 +8,7 @@ packages=(
     'pm:python-requests'
 )
 
-links=( "${ADM_DIR}/config" "${HOME}/.config/radicale/config")
+links=( "${ADM_DIR}/config" "${HOME}/.config/radicale/config" )
 
 st_install() {
     sudo install -m644 -D --owner=root --group=root "${ADM_DIR}/radicale@.service" '/etc/systemd/system/'
