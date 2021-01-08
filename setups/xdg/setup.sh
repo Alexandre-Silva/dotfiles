404038b416e8534a757cad9af3327440d46759e9
Alexandre-Silva <alexandre.s.silva@posteo.net>
2021-01-08 14:33:46 +0000

    feat(xdg): ch install to create dirs if missing

--- setups/xdg/setup.sh
+++ setups/xdg/setup.sh
@@ -12,22 +12,41 @@ links=(
 st_profile() {
     export XDG_CONFIG_HOME="$HOME/.config"
 
-    local NAMES=(
-        DESKTOP
-        DOWNLOAD
-        TEMPLATES
-        PUBLICSHARE
-        DOCUMENTS
-        MUSIC
-        PICTURES
-        VIDEOS
-    )
+    export XDG_DESKTOP_DIR
+    export XDG_DOWNLOAD_DIR
+    export XDG_TEMPLATES_DIR
+    export XDG_PUBLICSHARE_DIR
+    export XDG_DOCUMENTS_DIR
+    export XDG_MUSIC_DIR
+    export XDG_PICTURES_DIR
+    export XDG_VIDEOS_DIR
 
     source "$XDG_CONFIG_HOME/user-dirs.dirs"
 
-    for name in "${NAMES[@]}"; do
-        export "XDG_${name}_DIR"
-    done
-
     xdg-user-dirs-update
+}
+
+st_install() {
+    export XDG_CONFIG_HOME="$HOME/.config"
+
+    export XDG_DESKTOP_DIR
+    export XDG_DOWNLOAD_DIR
+    export XDG_TEMPLATES_DIR
+    export XDG_PUBLICSHARE_DIR
+    export XDG_DOCUMENTS_DIR
+    export XDG_MUSIC_DIR
+    export XDG_PICTURES_DIR
+    export XDG_VIDEOS_DIR
+
+    source "$XDG_CONFIG_HOME/user-dirs.dirs"
+
+    mkdir -vp $XDG_DESKTOP_DIR
+    mkdir -vp $XDG_DOWNLOAD_DIR
+    mkdir -vp $XDG_TEMPLATES_DIR
+    mkdir -vp $XDG_PUBLICSHARE_DIR
+    mkdir -vp $XDG_DOCUMENTS_DIR
+    mkdir -vp $XDG_MUSIC_DIR
+    mkdir -vp $XDG_PICTURES_DIR
+    mkdir -vp $XDG_VIDEOS_DIR
+
 }
