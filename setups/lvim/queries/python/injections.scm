1ef2eef7332d143df1069c56fc7c84da1b2a57d3
Alexandre-Silva <nowhere@example.com>
2022-12-04 12:31:00 +0000

    lvim: now auto highlights SQL inside text('...') functions

--- setups/lvim/queries/python/injections.scm
+++ setups/lvim/queries/python/injections.scm
@@ -1,4 +1,11 @@
-; Highlight embbeded SQL. Strings must have '-- TS-sql' OR be inside a text() function
-
+; Highlight embbeded SQL. Strings that have '-- TS-sql' in them
 ((string) @sql (#contains? @sql "-- TS-sql"))
+
+
+; Highlight SQL inside text() functions
+((call
+  function: (identifier) @_fn
+  arguments: (argument_list
+    (string) @sql))
+  (#any-of? @_fn "text" "TEXT"))
 
