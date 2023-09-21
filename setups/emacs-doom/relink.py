fff7260b6f6ee6f2eeef020eb227a560ec4c960f
Alexandre-Silva <nowhere@example.com>
2023-09-21 12:30:40 +0100

    emacs-doom: more links to relink.py

--- setups/emacs-doom/relink.py
+++ setups/emacs-doom/relink.py
@@ -29,6 +29,11 @@ REPLACE = {
     '[[GS]]': '[[id:fe350e80-670d-4ea0-939f-ae1798e25e41][GS]]',
     '[[GS/EMS]]': '[[id:6efa8442-0239-4df4-b326-1dc10a55c749][EMS]]',
     '[[GS/SAT1]]': '[[id:3aac5e64-fd67-488d-a82c-fe4d9dbbadaa][GS-SAT1]]',
+    '[[Guide/Hand exercices]]': '[[id:69e549e8-a850-4e5b-867f-3889649c4c50][Hand Exercices]]',
+    '[[Ref/Groxy]]': '[[id:f1cea066-b020-4c62-9a85-57deefb1b438][Grocy]]',
+    '[[Someday]]': '[[id:0652a5ff-d044-4167-b217-8fd6ffa85af4][Someday]]',
+    '[[Ref/Django]]': '[[id:6301ec94-924a-4735-9c0c-b7c7f89891be][Django]]',
+    '[[Ref/Redis]]': '[[id:1c83765a-56d9-4df3-a65a-c3c3ecd2f66b][Redis]]',
     #
     '[[to/consider]]': '[[id:201a6008-ffba-49ee-a27e-c25dfb331497][to.consider]]',
     '[[to/discuss]]': '[[id:751798e1-1210-4828-964c-f536a4929c2b][to-discuss]]',
@@ -54,41 +59,65 @@ REPLACE = {
     '#dev-tools': '[[id:4ee253b3-2611-4898-812b-1927fad2e1fa][dev_tools]]',
     '[[product]]': '[[id:4aca2a21-db29-4c64-95b8-0c80eb9f5397][product]]',
     '#product': '[[id:4aca2a21-db29-4c64-95b8-0c80eb9f5397][product]]',
+
+    #
+    '[[2023-08-27 Sunday]]': '[[id:f73c7ef0-9a22-4c30-8c4c-595e463b24c1][2023-08-27]]',
+    '[[2023-09-04 Monday]]': '[[id:9f76d6d9-326f-4d2e-a763-2f463e352ede][2023-09-04]]',
+    '[[2023-09-08 Friday]]': '[[id:3127d1dd-8150-4af5-9592-229ec371f192][2023-09-08]]',
+    '[[2023-09-09 Saturday]]': '[[id:9f97f158-bbb1-484f-9c25-467f0bf7b979][2023-09-09]]',
+    '[[2023-09-11 Monday]]': '[[id:6adbcd53-419e-4283-ac0f-f6e5fb585efb][2023-09-11]]',
+    '[[2023-09-12 Tuesday]]': '[[id:bfa1bde7-f856-462e-9c1e-5d9b757e7743][2023-09-12]]',
+    '[[2023-09-13 Wednesday]]': '[[id:07743ae9-ada4-4376-b991-3bc75ba1add2][2023-09-13]]',
+    '[[2023-09-15 Friday]]': '[[id:43db7705-6a2e-4d4f-8f67-e0fa33ebda33][2023-09-15]]',
+    '[[2023-09-16 Saturday]]': '[[id:6f193995-d7cc-4e72-9195-1ff0c00f551c][2023-09-16]]',
+    '[[2023-09-18 Monday]]': '[[id:f76e44ae-b347-43f2-83c2-848ad05214a0][2023-09-18]]',
 }
 
 
 def main():
     cmd = sys.argv[1]
-    target = sys.argv[2]
+    targets = sys.argv[2:]
 
     if cmd == 'search':
-        link = '[[Ref/WhatsApp]]'
-        p = re.escape(link)
         p = r'\[\[[a-zA-Z0-9\s\/-]*\]\]'
+        counter = collections.Counter()
+        entry2file = {}
 
-        with open(target, 'r') as f:
-            text = '\n'.join(f.readlines())
+        for target in targets:
+            with open(target, 'r') as f:
+                text = '\n'.join(f.readlines())
 
-        matches = re.findall(p, text)
-        counter = collections.Counter(matches)
-        for k, v in counter.items():
-            note = '' if k in REPLACE else '(no link)'
-            print(f'{v:2d} {k} {note}')
+            matches = re.findall(p, text)
+            counter.update(matches)
+            for m in matches:
+                if m not in entry2file:
+                    entry2file[m] = [target]
+                else:
+                    entry2file[m].append(target)
+
+        entries = [(k, v) for k, v in counter.items()]
+        entries.sort(key=lambda x: x[0])
+        for key, count in entries:
+            note = '' if key in REPLACE else '(no link)'
+            files = ','.join(entry2file.get(key, []))
+            print(f'{count:2d} {key} {note}: {files}')
 
     elif cmd == 'replace':
-        with open(target, 'r+') as f:
-            text = f.read()
+        for target in targets:
+            print(f'> {target}')
+            with open(target, 'r+') as f:
+                text = f.read()
 
-            for k, v in REPLACE.items():
-                text = re.sub(r'#?' + re.escape(k), v, text)
+                for k, v in REPLACE.items():
+                    text = re.sub(r'#?' + re.escape(k), v, text)
 
-            # fp = Path(target)
-            # fp = fp.with_stem(fp.stem + '.2')
-            # print(fp)
+                # fp = Path(target)
+                # fp = fp.with_stem(fp.stem + '.2')
+                # print(fp)
 
-            f.seek(0)
-            f.write(text)
-            f.truncate()
+                f.seek(0)
+                f.write(text)
+                f.truncate()
 
     else:
         assert False, f'invalid cmd {cmd}'
